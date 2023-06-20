603aea85bf9e6fb6853413ef3aa047c01c2cab36
Svetlana <azogam@gmail.com>
2023-06-20 11:24:46 +0300

    Condition_Else_If

---
+++ Example005_Condition_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurray!Masha!");
+}
+else
+{
+    Console.Write("Hi there!");
+    Console.WriteLine(username);
+}
